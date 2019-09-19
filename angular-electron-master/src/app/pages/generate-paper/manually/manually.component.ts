c56c290f4b957b349aeee6b6d47b5e2af22c2ba4
sutharjatindra@gmail.com <Jatin@52846>
2019-09-19 23:14:33 +0530

    Generate paper component

---
+++ angular-electron-master/src/app/pages/generate-paper/manually/manually.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manually',
+  templateUrl: './manually.component.html',
+  styleUrls: ['./manually.component.scss']
+})
+export class ManuallyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
